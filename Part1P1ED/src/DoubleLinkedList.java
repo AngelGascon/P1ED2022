cb1bf84793a83782cc5cfde02a4506dff41fa8ba
Àngel Gascón <angel.gascon@estudiants.urv.cat>
2022-02-18 19:41:53 +0100

    Functional DLL, testing needed.

--- Part1P1ED/src/DoubleLinkedList.java
+++ Part1P1ED/src/DoubleLinkedList.java
@@ -1,23 +1,21 @@
 import java.lang.Iterable;
 import java.util.Iterator;
 
-public class DoubleLinkedList<T> implements TADdLinkedList<T>, Iterable<T>{
+public class DoubleLinkedList<T> implements TADdLinkedList<T>, Iterable<T> {
 
     private int size;
     private DoubleLinkedListNode head, tail;
 
-    DoubleLinkedList(){
+    public DoubleLinkedList(){
         create();
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return new DllIterator(head);
-    }
+    public Iterator<T> iterator() { return new DllIterator(head); }
 
     private class DllIterator implements Iterator<T>{
 
-        private DoubleLinkedListNode current;
+        protected DoubleLinkedListNode current;
 
         public DllIterator(DoubleLinkedListNode name){
             current = name;
@@ -30,16 +28,18 @@ public class DoubleLinkedList<T> implements TADdLinkedList<T>, Iterable<T>{
 
         @Override
         public T next() {
-            T info = current.getInfo(); //TODO why requires casting
+            T info = current.getInfo();
             current = current.next;
             return info;
         }
+
+        public DoubleLinkedListNode getCurrent(){ return current; }
     }
 
-    private class DoubleLinkedListNode {
+    private class DoubleLinkedListNode implements Comparable<T> {
 
-        DoubleLinkedListNode prev, next;
-        T info;
+        protected DoubleLinkedListNode prev, next;
+        protected T info;
 
         DoubleLinkedListNode(T info){
             this.info = info;
@@ -48,6 +48,13 @@ public class DoubleLinkedList<T> implements TADdLinkedList<T>, Iterable<T>{
         }
 
         public T getInfo() { return info; }
+
+        @Override
+        public int compareTo(T o) {
+            // returns a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+            // Using compareTo defined in java.lang.String, viable to look for elements that are equal
+            return (info.toString().compareTo(o.toString()));
+        }
     }
 
     @Override
@@ -63,39 +70,76 @@ public class DoubleLinkedList<T> implements TADdLinkedList<T>, Iterable<T>{
         if(size == 0){
             head.next = aux;
             aux.prev = head;
-            aux.next = tail;
-            tail.prev = aux;
         }else{
             aux.prev = tail.prev;
             tail.prev.next = aux;
-            aux.next = tail;
-            tail.prev = aux;
         }
+        aux.next = tail;
+        tail.prev = aux;
         size++;
     }
 
     @Override
     public void add(int pos, T elem) {
-
+        try{
+            DoubleLinkedListNode inAux = new DoubleLinkedListNode(elem);
+            DoubleLinkedListNode itAux = this.getListNode(pos);
+            //Adding new node pointers
+            inAux.next = itAux;
+            inAux.prev = itAux.prev;
+            //Updating current node pointers
+            itAux.prev.next = inAux;
+            itAux.prev = inAux;
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public T get(int pos) {
-        return null;
+        return this.getListNode(pos).getInfo();
     }
 
     @Override
     public int lon() {
-        return 0;
+        return size;
     }
 
     @Override
     public void delete(int pos) {
-
+        try {
+            DoubleLinkedListNode aux = this.getListNode(pos);
+            aux.prev.next = aux.next;
+            aux.next.prev = aux.prev;
+            size--;
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public int search(T elem) {
-        return 0;
+        DllIterator it = new DllIterator(head);
+        int aux = 0;
+        boolean found = false;
+
+        while (it.hasNext() && !found){
+            it.next(); aux++;
+            if(it.getCurrent().compareTo(elem)==0) found =true;//TODO needs to start comparing after it.next(), first iteration is head and throws nullPointerException
+        }
+        return aux;
+    }
+
+    /**
+     * Returns node reference at the specified position.
+     * If pos > than current number of nodes, returns tail reference.
+     * @param pos int
+     * @return DoubleLinkedListNode
+     */
+    private DoubleLinkedListNode getListNode(int pos){
+        DllIterator it = new DllIterator(head);
+        int aux = 0;
+        while (it.hasNext() && aux <= pos){ it.next(); aux++; }
+        return it.getCurrent();
     }
 }
--- Part1P1ED/src/TestDLL.java
+++ Part1P1ED/src/TestDLL.java
@@ -6,13 +6,19 @@ public class TestDLL {
 
         provaLinked.add(22);
         provaLinked.add(30);
-        provaLinked.add(30);
-        provaLinked.add(30);
+        provaLinked.add(31);
+        provaLinked.add(32);
+        provaLinked.add(6, 33);
+        provaLinked.create();
+        System.out.println(".get test: " + provaLinked.get(3));
+        System.out.println(".lon test: " + provaLinked.lon());
+        provaLinked.delete(3);
+        System.out.println(".lon test2: " + provaLinked.lon());
+        System.out.println(".search test: " + provaLinked.search(33));
 
-        Iterator iterLinked = provaLinked.iterator();
-
-        while (iterLinked.hasNext()){
-            System.out.println(iterLinked.next());
+        Iterator itLinked = provaLinked.iterator();
+        while (itLinked.hasNext()){
+            System.out.println(itLinked.next());
         }
     }
 }
