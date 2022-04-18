16b2931a04d67845153e79982c3c868ad5dca766
Àngel Gascón <angel.gascon@estudiants.urv.cat>
2022-04-18 13:15:35 +0200

    testing finished P1

--- Part1P1ED/src/TestDLL.java
+++ Part1P1ED/src/TestDLL.java
@@ -4,22 +4,35 @@ public class TestDLL {
         Citizen num1 = new Citizen("Maria", "Garcia", "334");
         Citizen num2 = new Citizen("Alvaro", "Martin", "124");
         Citizen num3 = new Citizen("Alfred", "Tomas", "204");
-        Citizen num4 = new Citizen("Marta", "Vent", "112");
-
+        Citizen num4 = new Citizen("Marta", "Vent", "110");
+        Citizen num5 = new Citizen("Ruben", "Roda", "112");
+        Citizen num6 = new Citizen("Gemma", "Reverte", "122");
+        Citizen num7 = new Citizen("Marta", "Sancho", "134");
+        Citizen num8 = new Citizen("Daniel", "Rius", "178");
+        Citizen num9 = new Citizen("Àlex", "Casanova", "200");
+        Citizen num10 = new Citizen("Pere", "Rullo", "300");
+        //adding citizens
         provLinked.add(num1);
         provLinked.add(num2);
         provLinked.add(num3);
         provLinked.add(num4);
-        provLinked.add(3, num1);
-
-        System.out.println(".get test: " + provLinked.get(3));
-        System.out.println(".lon test: " + provLinked.lon());
-        provLinked.delete(3);
-        System.out.println(".lon test2: " + provLinked.lon());
-        System.out.println(".search test: " + provLinked.search(num2));
-
+        provLinked.add(num5);
+        provLinked.add(num6);
+        provLinked.add(num7);
+        provLinked.add(num8);
+        provLinked.add(num9);
+        provLinked.add(num10);
+        provLinked.add(0, num1);
+        //Printing DLL
+        System.out.println("Printing DoubleLinkedList, Expected result: Every citizen + Maria at position 0 -> Maria(LastAdd), Maria, Alvaro, Alfred, Marta Vent, Ruben, Gemma, Marta Sancho, Daniel, Àlex, Pere");
         for (Citizen x:provLinked) {
             System.out.println(x);
         }
+        //Testing operations
+        System.out.println(".get(3), Expected result: Alfred Tomas 204 ->  " + provLinked.get(3));
+        System.out.println(".lon(), Expected result: 10 -> " + provLinked.lon());
+        provLinked.delete(3);
+        System.out.println(".lon(), Expected result: 9 -> " + provLinked.lon());
+        System.out.println(".search(), Expected result: O(n) -> " + provLinked.search(num6));
     }
 }
--- Part2P1ED/src/TestHashList.java
+++ Part2P1ED/src/TestHashList.java
@@ -5,30 +5,55 @@ public class TestHashList {
         Citizen num1 = new Citizen("Maria", "Garcia", "334");
         Citizen num2 = new Citizen("Alvaro", "Martin", "124");
         Citizen num3 = new Citizen("Alfred", "Tomas", "204");
-        Citizen num4 = new Citizen("Marta", "Vent", "112");
-
-        testTable.insert(345, num1);
-        testTable.insert(340, num2);
-        testTable.insert(346, num3);
-        testTable.insert(3456, num4);
-        testTable.insert(56, num3);
-        testTable.insert(2, num4);
-        testTable.insert(3, num4);
-        testTable.insert(4, num4);
-        testTable.insert(5, num4);
-        System.out.println(testTable.get(346));
-        testTable.delete(346);
-        System.out.println(testTable.get(346));
-        System.out.println(testTable.lon());
-        System.out.println(testTable.search(346));
-
-        DoubleLinkedList<Integer> aux = testTable.getKeys();
-        for (Integer obj : aux) {
-            System.out.println(obj);
-        }
+        Citizen num4 = new Citizen("Marta", "Vent", "110");
+        Citizen num5 = new Citizen("Ruben", "Roda", "112");
+        Citizen num6 = new Citizen("Gemma", "Reverte", "122");
+        Citizen num7 = new Citizen("Marta", "Sancho", "134");
+        Citizen num8 = new Citizen("Daniel", "Rius", "178");
+        Citizen num9 = new Citizen("Àlex", "Casanova", "200");
+        Citizen num10 = new Citizen("Pere", "Rullo", "300");
+        //Init Hash Table
+        testTable.insert(334, num1);
+        testTable.insert(124, num2);
+        testTable.insert(204, num3);
+        testTable.insert(110, num4);
+        testTable.insert(112, num5);
+        testTable.insert(122, num6);
+        testTable.insert(134, num7);
+        testTable.insert(178, num8);
+        testTable.insert(200, num9);
+        testTable.insert(300, num10);
+        //Printing HashTable
+        System.out.println("Printing HashTable, Expected result: Every citizen -> Maria, Alvaro, Alfred, Marta Vent, Ruben, Gemma, Marta Sancho, Daniel, Àlex, Pere");
         DoubleLinkedList<Citizen> aux2 = testTable.getValues();
         for (Citizen obj : aux2) {
             System.out.println(obj);
         }
+        //Get (Buscar)
+        System.out.println(".get(334), Expected result: Maria Garcia 334 -> "+testTable.get(334));
+        System.out.println(".get(300), Expected result: Pere Rullo 300 -> "+testTable.get(300));
+        System.out.println(".get(3300), Expected result: Error code null -> "+testTable.get(3300));
+        //lon (Mida)
+        System.out.println(".lon(), Expected result: 10 -> "+testTable.lon());
+        //delete(Esborrar)
+        testTable.delete(300);
+        System.out.println(".lon(), Expected result: 9 (Pere is deleted) -> "+testTable.lon());
+        //Printing HashTable again
+        System.out.println("Printing HashTable, Expected result: Every citizen -> Maria, Alvaro, Alfred, Marta Vent, Ruben, Gemma, Marta Sancho, Daniel, Àlex, Pere(deleted)");
+        aux2 = testTable.getValues();
+        for (Citizen obj : aux2) {
+            System.out.println(obj);
+        }
+        System.out.println("Printing HashTable keys, Expected result: Every citizen key(id) -> Maria, Alvaro, Alfred, Marta Vent, Ruben, Gemma, Marta Sancho, Daniel, Àlex, Pere(deleted)");
+        DoubleLinkedList<Integer> aux = testTable.getKeys();
+        for (Integer obj : aux) {
+            System.out.println(obj);
+        }
+        //.getLoadFactor (ObtenirFactorDeCarrega)
+        System.out.println(".getLoadFactor(), Expected result: (10.0 to 9.0, Pere got deleted) 9.0/20, 0.45 -> "+testTable.getLoadFactor());
+        System.out.println(".search(), Expected result: O(1) -> "+ testTable.search(124));
+        System.out.println(".search(), Expected result: O(1) -> "+ testTable.search(334));
+        System.out.println(".search(), Expected result: O(1) -> "+ testTable.search(178));
+        System.out.println(".search(), Expected result: O(1) -> "+ testTable.search(200));
     }
 }
